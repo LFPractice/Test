98da93dd0dad8c35ed2e8430e2745781d92aae53
WangLinfang <1261876974@qq.com>
2019-04-11 14:27:09 +0800

    xxx

---
+++ ComponentTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ComponentTest
+//
+//  Created by 王林芳 on 2019/4/11.
+//  Copyright © 2019 王林芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ComponentTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ComponentTest
+//
+//  Created by 王林芳 on 2019/4/11.
+//  Copyright © 2019 王林芳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
